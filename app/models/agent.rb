edac25cf7792e4a1fc83fa9b2a758c053d922b35
James Yeates <jimibue@gmail.com>
2020-04-07 16:44:54 -0600

    comments

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,6 +2,7 @@ class Agent < ApplicationRecord
   has_many :buyers
   has_many :properties
 
+  # Note: The != operator is converted to <> in the parser stage. It is not possible to implement != and <> operators that do different things.
   def self.unsold_homes
     select("agents.id, first_name, last_name, email, sold, COUNT(*) as frequency")
       .joins("INNER JOIN properties p ON p.agent_id = agents.id")
